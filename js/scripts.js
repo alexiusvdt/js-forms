4430b7b3e1fe530969c8ace7fdfabc82cbb740ea
Alex Johnson <alex.johnson293@gmail.com>
2022-10-19 10:21:15 -0700

    initialize project with debugging practice

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    // new line below
+    // console.log("verbInput = " + verbInput);
+    const nounInput = document.getElementById("nounInput").value;
+
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    // new line below
+    // console.log("targeting span in story =", document.querySelector("span#verb").innerTex)
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
+
+    document.querySelector("div#story").removeAttribute("class");
+  };
+};
